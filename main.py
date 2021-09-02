ae2568b443b1ac323e27c70739d8ee2807b18b79
Ivan Orlov <pin88@bk.ru>
2021-09-02 22:17:48 +0300

    Correction PEP8

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import time
 from datetime import datetime
 import json
-
 from tqdm import tqdm
+
 
 class VKFotos():
     url = 'https://api.vk.com/method/'
@@ -23,7 +23,8 @@ class VKFotos():
             'album_id': 'profile'
             }
 
-        response = requests.get(photos, params={**self.params, **photos_params}).json()
+        response = requests.get(photos, params={**self.params,
+                                                **photos_params}).json()
         list_photos = response['response']['items']
 
         for k in list_photos:
@@ -37,7 +38,7 @@ class VKFotos():
             for sizes in k['sizes']:
                 size = sizes['height'] * sizes['width']
                 if size > max_size:
-                    x ={}
+                    x = {}
                     max_size = size
                     url = sizes['url']
                     x[sizes['type']] = url
@@ -57,28 +58,35 @@ class YAfotos():
 
     def create_folder(self):
         photos_dict = up.get_photos()
-        list =[1]
+        list = [1]
         list_json = []
         name_folder = input('Введите название папки: ')
-        requests.put(f'{self.url}', params={'path': {name_folder}}, headers=self.headers)
+        requests.put(f'{self.url}', params={'path': {name_folder}},
+                      headers=self.headers)
         for photos in photos_dict:
             name_photo = str(photos)
             for key in photos_dict[photos]:
-                response = requests.post(self.url + 'upload', headers=self.headers,
-                                         params={'path': f'{name_folder}/{name_photo}.jpg',
-                                                 'url': photos_dict[photos][key]})
+                response = requests.post(self.url + 'upload',
+                                         headers=self.headers,
+                                         params={'path': f'{name_folder}/'
+                                                         f'{name_photo}.jpg',
+                                                 'url':
+                                                     photos_dict[photos][key]})
 
-                if response.ok== True:
-                   for i in tqdm(list):
+                if response.ok:
+                    for i in tqdm(list):
                         time.sleep(1)
-                        list_json.append({"file_name":f'{name_photo}.jpeg', "size":key})
-                   else:
-                       pass
+                        list_json.append({"file_name": f'{name_photo}.jpeg',
+                                         "size": key})
+                    else:
+                        pass
         with open('photo.json', 'w') as outfile:
             json.dump(list_json, outfile)
 
 token_ya = input("Введите токен яндекс диска: ")
-up = VKFotos('958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008', '5.131')
+token_vk = '958eb5d439726565e9333aa30e50e0f937ee43' \
+           '2e927f0dbd541c541887d919a7c56f95c04217915c32008'
+up = VKFotos(token_vk, '5.131')
 puting = YAfotos(token_ya)
 puting.create_folder()
 
